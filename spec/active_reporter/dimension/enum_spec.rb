f96b366ec42587e3f5db628d7c3553e2883ce04f
chaunce <chaunce.slc@gmail.com>
2020-11-16 17:36:27 -0700

    add enum dimension specs

--- spec/active_reporter/dimension/enum_spec.rb
+++ spec/active_reporter/dimension/enum_spec.rb
@@ -2,98 +2,46 @@ require 'spec_helper'
 
 describe ActiveReporter::Dimension::Enum do
   let(:report_model) { :post }
-  let(:report) do
-    OpenStruct.new(
-      params: { dimensions: { groupers: [:status] } }, 
-      groupers: [:status, :category], 
-      raw_data: {
-        ['published', 'post_count'] => 5, ['published', 'post_total'] => 500.00, ['published', 'post_average'] => 100.00,
-        ['archived', 'post_count'] => 7, ['archived', 'post_total'] => 530.25, ['archived', 'post_average'] => 75.75,
-      },
-    )
-  end
+  let(:filter_values) { nil }
+  let(:status_dimension_options) { { only: filter_values }.compact }
+  let(:report) { OpenStruct.new(params: { dimensions: { status: status_dimension_options } }, groupers: [:status, :category], raw_data: raw_data) }
 
+  let(:raw_data) { {
+    ['published', 'post_count'] => 5, ['published', 'post_total'] => 500.00, ['published', 'post_average'] => 100.00,
+    ['archived', 'post_count'] => 7, ['archived', 'post_total'] => 530.25, ['archived', 'post_average'] => 75.75,
+  } }
+
+  let(:enum_values) { { 'draft' => 0, 'unpublished' => 1, 'published' => 2, 'archived' => 3 } }
+  let(:group_values) { ['published', 'archived'] }
+  let(:all_values) { enum_values.keys.unshift(nil) }
+  
   let(:status_dimension) do
-    dimension = ActiveReporter::Dimension::Enum.new(:status, report, { model: report_model })
+    dimension = ActiveReporter::Dimension::Enum.new(:status, report, { model: report_model, only: filter_values })
+    allow(dimension).to receive(:enum_values).and_return(enum_values)
     report.groupers[report.groupers.index(:status)] = dimension if report.groupers.include?(:status)
+
     dimension
   end
 
-  # def status_dimension(report)
-  #   described_class.new(:status, report, model: :posts, attribute: :status)
-  # end
-
-  # def author_dimension(report)
-  #   described_class.new(:author, report, model: :authors, attribute: :name, relation: ->(r) { r.joins(
-  #     "LEFT OUTER JOIN authors ON authors.id = posts.author_id") })
-  # end
-
   describe '#group_values' do
-    # it 'echoes filter_values if filtering' do
-    #   dimension = status_dimension(OpenStruct.new(params: {
-    #     groupers: [:status]
-    #   }))
-    #   expect(dimension.group_values).to eq [nil, 'draft', 'unpublished', 'published', 'archived']
-    # end
+    context 'when filtering' do
+      let(:filter_values) { ['unpublished', 'published', 'archived'] }
 
+      it 'returns filter_values' do
+        expect(status_dimension).to be_filtering
+        expect(status_dimension.group_values).to eq filter_values
+      end
+    end
 
-    it 'determines #filtering?' do
+    it 'returns group enum values' do
       expect(status_dimension).not_to be_filtering
-      expect(status_dimension.group_values).to match %w(published archived)
+      expect(status_dimension.group_values).to match group_values
     end
   end
 
-  # describe '#all_values' do
-  #   it 'returns model enum values' do
-  #     dimension = author_dimension(OpenStruct.new(params: {
-  #       dimensions: { author: { only: [nil, 'draft', 'unpublished', 'published', 'archived'] } }
-  #     }))
-  #     expect(dimension.all_values).to eq described_class.
-  #   end
-  # end
-
-  # describe '#filter' do
-  #   it 'filters to rows matching at least one value' do
-  #     p1 = create(:post, author: 'Alice')
-  #     p2 = create(:post, author: 'Bob')
-  #     p3 = create(:post, author: nil)
-
-  #     def filter_by(author_values)
-  #       report = OpenStruct.new(
-  #         table_name: 'posts',
-  #         params: { dimensions: { author: { only: author_values } } }
-  #       )
-  #       dimension = author_dimension(report)
-  #       dimension.filter(dimension.relate(Post))
-  #     end
-
-  #     expect(filter_by(['Alice'])).to eq [p1]
-  #     expect(filter_by([nil])).to eq [p3]
-  #     expect(filter_by(['Alice', nil])).to eq [p1, p3]
-  #     expect(filter_by(['Alice', 'Bob'])).to eq [p1, p2]
-  #     expect(filter_by([])).to eq []
-  #   end
-  # end
-
-  # describe '#group' do
-  #   it 'groups the relation by the exact value of the SQL expression' do
-  #     p1 = create(:post, author: 'Alice')
-  #     p2 = create(:post, author: 'Alice')
-  #     p3 = create(:post, author: nil)
-  #     p4 = create(:post, author: 'Bob')
-  #     p5 = create(:post, author: 'Bob')
-  #     p6 = create(:post, author: 'Bob')
-
-  #     report = OpenStruct.new(table_name: 'posts', params: {})
-  #     dimension = author_dimension(report)
-
-  #     results = dimension.group(dimension.relate(Post)).select("COUNT(*) AS count").map do |r|
-  #       r.attributes.values_at(dimension.send(:sql_value_name), 'count')
-  #     end
-
-  #     expect(results).to eq [[nil, 1], ['Alice', 2], ['Bob', 3]]
-  #   end
-  # end
-
-  
+  describe '#all_values' do
+    it 'returns model enum values' do
+      expect(status_dimension.all_values).to match all_values
+    end
+  end
 end
