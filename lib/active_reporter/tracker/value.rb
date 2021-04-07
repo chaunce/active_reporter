1158be1b196a7fcd37dc184b2002457610f495cd
chaunce <chaunce.slc@gmail.com>
2021-04-06 20:31:07 -0600

    bug fixes

--- lib/active_reporter/tracker/value.rb
+++ lib/active_reporter/tracker/value.rb
@@ -2,7 +2,7 @@ module ActiveReporter
   module Tracker
     class Value < ActiveReporter::Tracker::Base
       def track(_, prior_row)
-        prior_row[prior_aggregator] if prior_row.nil?
+        prior_row[aggregator] if prior_row.nil?
       end
     end
   end
